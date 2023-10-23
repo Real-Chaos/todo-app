fe9f7ef4907154e3d653c372b317a5573c633d56
Real-Chaos <rizwannafees13@gmail.com>
2023-10-23 18:56:21 -0400

    new projects can be added to side nav and tasks can be properly managed

--- handlingDom.js
+++ handlingDom.js
@@ -51,7 +51,7 @@ const toggleTasksForm = (function () {
 
   toggler.addEventListener('click', () => {
     div.style.display = 'block'
-    addTask()
+    
   })
 
   cancelTask.addEventListener('click', () => {
@@ -73,15 +73,15 @@ const displayTasks = (tasks) => {
       <h3>${task.name}</h3>
     </div>
     <div class="extra-options">
-      <button class="details-btn">Details</button>
+      <button class="details-btn" onclick="openModal(this)">Details</button>
       <h5>${task.date}</h5>
-      <i class="fa-solid fa-user-pen edit-task-btn"></i>
-      <i class="fa-solid fa-trash-can delete-task-btn"></i>
+      <i class="fa-solid fa-user-pen edit-task-btn" onclick="editTask(this)"></i>
+      <i class="fa-solid fa-trash-can delete-task-btn" onclick="deleteTask(this)"></i>
     </div>
     <dialog>
       <div>
         <h3>${task.name}</h3>
-        <i class="fa-regular fa-circle-xmark close-dialog"></i>
+        <i class="fa-regular fa-circle-xmark close-dialog" onclick="closeModal(this)"></i>
       </div>
       <div class="real-details">
         <div>
@@ -109,3 +109,83 @@ const displayTasks = (tasks) => {
     tasksDiv.innerHTML += html
   })
 }
+
+
+// ----------------------------- DISPLAY TASKS DETAILS ------------------------------ //
+
+
+const openModal = function (e) {
+  console.log(e.parentElement.parentElement.getElementsByTagName('dialog')[0])
+  const dialog = e.parentElement.parentElement.getElementsByTagName('dialog')[0]
+  const closeButtons = document.querySelectorAll('.close-dialog')
+
+  // "Show the dialog" button opens the dialog modally
+  
+  dialog.showModal()
+
+  // closeButtons.forEach((closeButton, index) => {
+  //   closeButton.addEventListener('click', () => {
+  //     dialogs[index].close()
+  //   })
+  // })
+}
+
+
+const closeModal = function (e) {
+  console.log(e.parentElement.parentElement.getElementsByTagName('dialog')[0])
+  const dialog = e.parentElement.parentElement
+  const closeButtons = document.querySelectorAll('.close-dialog')
+
+  // "Show the dialog" button opens the dialog modally
+  
+  dialog.close()
+
+  // closeButtons.forEach((closeButton, index) => {
+  //   closeButton.addEventListener('click', () => {
+  //     dialogs[index].close()
+  //   })
+  // })
+}
+
+
+// ----------------------------- EDIT TASK FROM DIV------------------------------ //
+
+const editTaskFormDOM = (task) => {
+  const formDiv = document.querySelector('.edit-tasks-form-div')
+  const form = document.querySelector('.edit-task-form')
+
+  formDiv.style.display = 'block'
+  form.elements.taskName.value = task.name
+  form.elements.description.value = task.description
+  form.elements.date.value = task.date
+  form.elements.priority.value = task.priority
+
+  
+
+}
+
+
+
+// ----------------------------- ADD PROJECT TO NAV ------------------------------ //
+const addProjectToSidenavDOM = (projects) => {
+  const projectSidebar = document.querySelector('.project-sidebar')
+  projects.forEach((project) => {
+    const html = `<div class="project-card project-name-div" data-index="0">
+    <div class="text">
+      <i class="fa-solid fa-list-check"></i>
+      <h4>${project.name}</h4>
+    </div>
+    <div class="options">
+      <i class="fa-solid fa-ellipsis-vertical options-toggler"></i>
+
+      <div class="options-text">
+        <h5>Rename</h5>
+        <h5>Delete</h5>
+      </div>
+    </div>
+  </div>`
+    projectSidebar.innerHTML += html
+  })
+
+}
+
--- main.js
+++ main.js
@@ -2,17 +2,40 @@
 
 const tasksObject = {
   tasks: [],
+  displayedTasks: [],
   getAllTasks: function () {
     return this.tasks
   },
+
   filterTasks: function (filter) {
     if (filter === 'All Tasks') {
       return this.getAllTasks()
     }
   },
+  getSpecificTask: function (filter) {
+    let returnTask = {}
+    this.tasks.forEach((task) => {
+      if (task.index === filter) {
+        returnTask = task
+      }
+    })
+
+    return returnTask
+  },
+  setEditedTask: function (editedTask) {
+    console.log(editedTask)
+    const filter = this.tasks.filter((task) => task.index != editedTask.index)
+    console.log(filter)
+    this.tasks = [...filter, editedTask]
+    return this.tasks
+  },
+  deleteTask: function(deleteTask) {
+    this.tasks = this.tasks.filter((task) => task.index != deleteTask.index)
+    return this.tasks
+  }
 }
 
-// ----------------------------- TASK CLASS---------------------------- //
+// ----------------------------- TASK AND PROJECT CLASS ---------------------------- //
 
 class newTask {
   constructor({ name, description, date, priority }) {
@@ -20,11 +43,32 @@ class newTask {
     this.description = description
     this.date = date
     this.priority = priority
+    this.index = tasksObject.getAllTasks().length
   }
 
   addToTaskList(task) {
     tasksObject.tasks.push(task)
   }
+}
+
+
+class projects {
+  constructor(name) {
+    this.name = name
+    this.projectsArray = []
+  }
+
+  addProject(project) {
+    this.projectsArray.push(project)
+    addProjectToSidenavDOM(this.projectsArray)
+  }
+}
+
+// ----------------------------- FUNCTIONS TO CALL------------------------------ //
+
+const recallFunctions = () => {
+  const tasksToDisplay = tasksObject.filterTasks('All Tasks')
+  displayTasks(tasksToDisplay)
 }
 
 // ----------------------------- ADD SUBMIT FORM ------------------------------ //
@@ -48,12 +92,69 @@ const addTask = function () {
       // ADD TASK TO LIST
       task.addToTaskList(task)
 
-      // DISPLAY THE RIGHT TASKS 
+      div.style.display = 'none'
       const tasksToDisplay = tasksObject.filterTasks('All Tasks')
       displayTasks(tasksToDisplay)
+      taskForm.reset()
+    },
+    { once: true }
+  )
+}
+
+// ----------------------------- EDIT TASK ------------------------------ //
+
+const editTask = (e) => {
+  let taskIndex = Number(
+    e.parentElement.parentElement.getAttribute('data-index')
+  )
+  const specificTask = tasksObject.getSpecificTask(taskIndex)
+  editTaskFormDOM(specificTask)
+
+  const taskForm = document.querySelector('.edit-task-form')
+  const div = document.querySelector('.edit-tasks-form-div')
+  console.log(taskForm)
+  taskForm.addEventListener(
+    'submit',
+    (e) => {
+      e.preventDefault()
+      const taskObj = {
+        name: e.target.elements.taskName.value,
+        description: e.target.elements.description.value,
+        date: e.target.elements.date.value,
+        priority: e.target.elements.priority.value,
+        index: specificTask.index,
+      }
+
+      console.log(taskObj)
+      displayTasks(tasksObject.setEditedTask(taskObj))
       div.style.display = 'none'
       taskForm.reset()
     },
     { once: true }
   )
 }
+
+// ----------------------------- DELETE TASK ------------------------------ //
+
+
+const deleteTask = (e) => {
+  let taskIndex = Number(
+    e.parentElement.parentElement.getAttribute('data-index')
+  )
+  const specificTask = tasksObject.getSpecificTask(taskIndex)
+  displayTasks(tasksObject.deleteTask(specificTask))
+}
+
+// ----------------------------- ADD PROJECT ------------------------------ //
+
+const addProjectToSidenav = (function () {
+  const addProjectForm = document.querySelector('.add-project-form')
+  const input = document.querySelector('#project-name')
+  addProjectForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const project = new projects(input.value)
+    project.addProject(project)
+    input.value = ''
+    toggleProjectOptions()
+  })
+})()
