f758e23a5fe686d292baf96588b91dc3454f6f7d
Real-Chaos <rizwannafees13@gmail.com>
2023-10-24 11:40:57 -0400

    now different priorities have different colors

--- handlingDom.js
+++ handlingDom.js
@@ -65,10 +65,11 @@ const displayTasks = (tasks) => {
   const tasksDiv = document.querySelector('.task-div')
   tasksDiv.innerHTML = ''
   tasks.forEach((task) => {
+    console.log(task.priority.replace(/ /g, ''))
     const html = `
     <div class="task" data-index="${task.index}">
     <div class="name">
-      <div class="task-completion completed"></div>
+      <div class="task-completion completed ${task.priority.replace(/ /g, '')}"></div>
       <h3>${task.name}</h3>
     </div>
     <div class="extra-options">
